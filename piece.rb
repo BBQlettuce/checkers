133bd743cdf695c898ffdffa184581b8fb6bec7f
Oscar Liu <oscar.liu.13q@gmail.com>
2015-08-06 10:06:47 -0400

    set forward directions for pieces depending on color

--- piece.rb
+++ piece.rb
@@ -1,8 +1,27 @@
 class Piece
 
-  def initialize(position)
+  UP_MOVE_DIRS = [[-1, -1], [-1, 1]]
+  DOWN_MOVE_DIRS = [[1, -1], [1, 1]]
+
+  attr_reader :color
+  attr_accessor :position
+
+  def initialize(position, color)
     @position = position
-    
+    # color either black or red, black's forward is up
+    @color = color
+  end
+
+  def forward_dirs
+    color == :black ? UP_MOVE_DIRS : DOWN_MOVE_DIRS
+  end
+
+  def perform_slide
+    # asks the board to do it, if possible
+  end
+
+  def perform_jump
+    # asks the board to do it
   end
 
 end
